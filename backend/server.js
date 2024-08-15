08e595ce7620cff171dd2e5b0863338b5f2fd13f
sarahi2426 <sarahicortes2426@gmail.com>
2024-08-15 00:27:01 -0600

    Fin de la segunda sesion

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT 
-
+const colors = require ('colors')
+const connectBD = require ('../config/db')
 const app = express()
+
+connectBD()
 
 app.use(express.json())
 
 app.use(express.urlencoded({extended:false}))
 
 app.use ('/api/tareas',require ('./routes/tareasRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, ()=> console.log (`servidor iniciado en el puerto ${port}`))
